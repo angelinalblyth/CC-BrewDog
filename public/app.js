abf1deb125e51e1f131b38b051c3f1afa45a0657
Angelina Blyth <angelinalblyth@hotmail.com>
2018-08-01 21:46:08 +0100

    Added some more beer info

--- public/app.js
+++ public/app.js
@@ -23,15 +23,15 @@ const populateList = function(beers){
     selectTag.appendChild(option);
   });
 
-  var storedBeer = localStorage.getItem('selected_beer');
-  var beerDetails = document.getElementById('beerDetails');
-  if(storedBeer){
-  beerToDisplay = JSON.parse(storedBeer);
-  console.log(storedBeer);
-  var pTag = document.createElement('p');
-  pTag.innerText = beerToDisplay.name;
-  beerDetails.appendChild(pTag);
-}
+//   var storedBeer = localStorage.getItem('selected_beer');
+//   var beerDetails = document.getElementById('beerDetails');
+//   if(storedBeer){
+//   beerToDisplay = JSON.parse(storedBeer);
+//   console.log(storedBeer);
+//   var pTag = document.createElement('p');
+//   pTag.innerText = beerToDisplay.name;
+//   beerDetails.appendChild(pTag);
+// }
 };
 
 
@@ -50,6 +50,19 @@ const showBeerDetails = function(beer){
   console.log(beer);
   pTag.innerText = beer.name;
   beerDetails.appendChild(pTag);
+
+
+  var imgHolder = document.createElement('img');
+  imgHolder.src = beer.image_url;
+  beerDetails.appendChild(imgHolder);
+
+  var abv = document.createElement('p');
+  abv.innerText = beer.abv;
+  beerDetails.appendChild(abv);
+
+  var description = document.createElement('p');
+  description.innerText = beer.description;
+  beerDetails.appendChild(description);
 
   var jsonString = JSON.stringify(beer);
   localStorage.setItem('selected_beer', jsonString);

2fd1a10069c3256618b3344e5970e8ad2b00d416
Angelina Blyth <angelinalblyth@hotmail.com>
2018-08-01 17:46:25 +0100

    Able to show beers in dropdown list

--- start_point/public/app.js
+++ start_point/public/app.js
@@ -1,5 +1,32 @@
-var app = function(){
+const makeRequest = function(url, callback){
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.addEventListener('load', callback);
+  request.send();
+}
+const requestComplete = function(){
+  if(this.status !== 200)return;
+  const jsonString = this.responseText;
+  const beers = JSON.parse(jsonString);
+  populateList(beers);
+  handleSelected(beers);
+}
 
+const populateList = function(beers){
+  let selectTag = document.getElementById('beerDropdown');
+
+    beers.forEach(function(beer, index){
+      let option = document.createElement('option');
+      option.className = 'select-result'
+      option.value = index;
+      option.innerText = beer.name;
+      selectTag.appendChild(option);
+    });
+  }
+
+var app = function(){
+  const url = 'https://api.punkapi.com/v2/beers';
+  makeRequest(url, requestComplete);
 }
 
 window.addEventListener('load', app);
